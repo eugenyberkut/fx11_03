e188dfec81403718f664d1abf5913c3d1d51b647
eugenyberkut <eugeny.berkunsky@gmail.com>
2016-03-18 12:51:35 +0200

    TableView

--- src/dbgui/controller/Controller.java
+++ src/dbgui/controller/Controller.java
@@ -1,9 +1,15 @@
 package dbgui.controller;
 
 import dbgui.model.AvtorService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import tables.Avtor;
 
 import javax.persistence.EntityManager;
@@ -19,9 +25,14 @@ public class Controller implements Initializable {
     public TextField avtorNameTextField;
     public TextField avtorCommentTextField;
 
-    EntityManagerFactory emf;
-    EntityManager em;
-    AvtorService as;
+    @FXML
+    TableView<Avtor> tableAvtors;
+    @FXML
+    TableColumn<Avtor,String> avtorNameColumn;
+    @FXML
+    TableColumn<Avtor,String> avtorCommentColumn;
+
+    private AvtorService as;
 
     public void doExit(ActionEvent actionEvent) {
         System.exit(0);
@@ -29,13 +40,24 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        emf = Persistence.createEntityManagerFactory("NewPU");
-        em = emf.createEntityManager();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("NewPU");
+        EntityManager em = emf.createEntityManager();
         as = new AvtorService(em);
+
+        avtorNameColumn.setCellValueFactory(new PropertyValueFactory<Avtor,String>("name"));
+        avtorCommentColumn.setCellValueFactory(new PropertyValueFactory<Avtor,String>("comment"));
+
     }
 
     public void showAvtors(ActionEvent actionEvent) {
-        as.findAll().stream().forEach(System.out::println);
+        showAvtors();
+    }
+
+    private void showAvtors() {
+        List<Avtor> avtors = as.findAll();
+        avtors.stream().forEach(System.out::println);
+        ObservableList<Avtor> ol = FXCollections.observableList(avtors);
+        tableAvtors.setItems(ol);
     }
 
 
@@ -45,5 +67,6 @@ public class Controller implements Initializable {
         Avtor avtor = as.create(name, comment);
         avtorNameTextField.setText("");
         avtorCommentTextField.setText("");
+        showAvtors();
     }
 }
